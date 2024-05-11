6f8867d5e3f0cf49c1588c163af43b1443588689
RogerGPacheco <rogergabriel.pacheco1@gmail.com>
2024-05-10 21:52:28 -0300

    adicionado tiro

---
+++ Assets/Scripts/ShotController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour
+{
+    public GameObject tiroPreFab;
+    public float forceSpeedFire;
+    public bool isPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && isPlayer)
+        {
+            Atirar();
+        }
+    }
+
+    public void Atirar()
+    {
+        GameObject tempTiro = Instantiate(tiroPreFab) as GameObject;
+        tempTiro.transform.position = transform.position;
+
+        tempTiro.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forceSpeedFire));
+    }
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -8,6 +8,8 @@ public class playerController : MonoBehaviour
     public Rigidbody2D playerRigidBody;
     public Transform playerTransform;
     public Animator playerAnimation;
+    public GameObject armaTripla;
+    public GameObject armaQuintopla;
 
     [Header("Speed")]
     public float maxSpeedX;
@@ -17,6 +19,8 @@ public class playerController : MonoBehaviour
     void Start()
     {
         playerAnimation = GetComponent<Animator>();
+        armaTripla.SetActive(false);
+        armaQuintopla.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,6 +31,23 @@ public class playerController : MonoBehaviour
 
         playerRigidBody.velocity = new Vector3(movX * maxSpeedX, movY * maxSpeedY);
         playerAnimation.SetFloat("movX", movX);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!armaTripla.activeSelf)
+            {
+                armaTripla.SetActive(true);
+            }
+            else if (!armaQuintopla.activeSelf)
+            {
+                armaQuintopla.SetActive(true);
+            }
+            else
+            {
+                armaTripla.SetActive(false);
+                armaQuintopla.SetActive(false);
+            }
+        }
         
     }
 }
